177dc95a5e6e7a5143f3f1113d0f3a2d292544c0
Elie Cohen <e.cohen@salesforce.com>
2018-11-14 22:17:17 +0200

    start working on http request

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,27 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', ['$scope', function($scope) {
+.controller('View1Ctrl', ['$scope','$location','$http', function($scope,$location,$http) {
+
+    //https://s3.amazonaws.com/mc2/web/pages/1.json
+
+    $http({
+        method : "GET",
+        url : "https://s3.amazonaws.com/mc2/web/pages/1.json?callback=JSON_CALLBACK"
+    }).then(function mySuccess(response) {
+        $scope.a = "44444444";
+        $scope.response = response;
+        console.log(response);
+        console.log("########################################");
+        $scope.data = response.data;
+    }, function myError(response) {
+        $scope.a = response;
+        console.log(response);
+    });
+
+
+    $scope.id = $location.search().id;
     $scope.title = "This is a tile";
+    $scope.text = "text.....";
+    $scope.chapter = "Chapter ABC";
 }]);
